ceb09dfd1befefbb68839b4df1e1988bd631fbdb
Mel Canis <mel.canis353@gmail.com>
2023-09-15 18:56:05 +0900

    image file name correction

--- src/app/shop/product.jsx
+++ src/app/shop/product.jsx
@@ -5,20 +5,20 @@ import product from "./product.module.sass"
 export default function Product ({n, name, price}) {
     return (
         <Link className={product.product} href={`/product/${name.replace(/#/g, "X").split(" ").join("-")}`}>
-            <div>
+            <div className={product.image}>
                 <Image
                 className={product.img}
-                src={`/products/${name.replace(/#/g, "-")}.jpg`}
+                src={`/products/${name.replace(/_/g, " ").replace(/#/g, "-")}.jpg`}
                 width={200}
                 height={300}
                 alt={name}
                 priority={n == 0 ? true : false}
                 />
+            </div>
                 <div className={product.span}>
                     <h3>{name}</h3>
                     <h5>{"$" + price}</h5>
                 </div>
-            </div>
         </Link>
     )
 }
